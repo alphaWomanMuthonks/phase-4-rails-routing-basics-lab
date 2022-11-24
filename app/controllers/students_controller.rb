e640463ee7fe5c8e054f9b823cae80e90db5f7f1
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-11-24 10:15:14 +0300

    generating routes and rendering Json

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+    def grades
+        students_grades = Student.all.order(grade: :desc)
+        render json: students_grades
+    end
+
+    def highest_grade
+        highest = Student.all.order(grade: :desc).first
+        render json: highest
+    end
+end
